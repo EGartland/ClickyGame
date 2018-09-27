a0ca10c9c9228b6a9b07cdca420b38bf73c9ea02
Dingus <35214896+EddieG77@users.noreply.github.com>
2018-09-27 14:09:40 -0500

    test

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,24 +5,29 @@ const NavBar = ({ correctGuesses, bestScore, clickMessage }) => {
 
 	return (
 		<nav className="navbar bg-danger justify-content-center flex-row">
-
+		<div class="container-fluid"></div>
+    	<div class="row">
+       		 <div class="col-md-1 col-sm-6">
+		
 			<a href='/' className="btn btn-outline-light btn-lg" role="button">Click the Pups!</a>
-
+			</div>
+			<div class="col-md-10 col-sm-6">
 			<div className="navbar-collapse">
 				<ul className="navbar-nav ml-auto flex-row justify-content-center">
-					<li className="nav-item">
+					<li className="nav-message">
 						<h4>{clickMessage}</h4>
 					</li>
 				</ul>
 			</div>
-
-			<ul className="nav navbar-nav ml-auto flex-row justify-content-end">
-				<li className="nav-item">
+			</div>
+			<div class="col-md-1 col-sm-6">
+			<ul className="navbar-nav ml-auto flex-row justify-content-end">
+				<li className="nav-score">
 					<h4>Score: {correctGuesses} < br />Top Score: {bestScore}</h4>
 				</li>
 			</ul>
-
-
+			</div>
+		</div>
 
 		</nav>
 	);
